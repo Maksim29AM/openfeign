82fc466d6a23d8eec01033b3171b7ddcb1b7f47f
Maksim29AM <maks.abramovich.1995@mail.ru>
2025-09-05 18:10:28 +0200

    Refine CrimsonSun request: increase result limit and filter by trackName

--- src/main/java/cam/example/app/AppApplication.java
+++ src/main/java/cam/example/app/AppApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.event.EventListener;
 
+import java.util.List;
+
 @SpringBootApplication
 @EnableFeignClients
 public class AppApplication {
@@ -20,9 +22,11 @@ public class AppApplication {
 
     @EventListener(ApplicationStartedEvent.class)
     public void makeRequestToCrimsonSunEndpoint() {
-        CrimsonSunResponse response = crimsonSunClient.makeSearchRequest("crimsonsun", 5);
-        System.out.println(response);
+        CrimsonSunResponse response = crimsonSunClient.makeSearchRequest("crimsonsun", 100);
+        List<CrimsonSunResult> results = response.results();
+        results.stream()
+                .filter(crimsonSunResult -> "Neon Lights".equalsIgnoreCase(crimsonSunResult.trackName()))
+                .forEach(System.out::println);
     }
-
 }
 
