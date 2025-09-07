8fa201fca36fd77092e93e237869bfbd64fe307f
Maksim29AM <maks.abramovich.1995@mail.ru>
2025-09-07 16:35:06 +0200

    Add deleteSong method

--- src/main/java/cam/example/app/sampleshawnmendesserver/SampleShawnMendesServerProxy.java
+++ src/main/java/cam/example/app/sampleshawnmendesserver/SampleShawnMendesServerProxy.java
@@ -1,22 +1,23 @@
 package cam.example.app.sampleshawnmendesserver;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 
 @FeignClient(value = "sample-server-shawn-mendes-client")
 public interface SampleShawnMendesServerProxy {
 
 
-
-//    https://localhost:8080/shawn/songs
+    //    https://localhost:8080/shawn/songs
     @GetMapping("/shawn/songs")
     SampleServerShawnMendesResponse fetchSongs();
 
 
     @PostMapping("/shawn/songs")
     SampleServerShawnMendesResponse addSong(@RequestBody SampleShawnMendesRequest request);
+
+    @DeleteMapping("/shawn/songs/{songId}")
+    void deleteSong(@PathVariable String songId);
+
 }
 
